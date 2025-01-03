f4c2136b7fdee4b41e70044146e0c210a816904a
Mweo Malupande <58066792+Mweo-Bruce@users.noreply.github.com>
2025-01-03 23:44:37 +0200

    Update and rename helloworld.rc to HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+// Aurthor: Mweo Malupande
+// Hello world in Java
+
+class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, world!!");
+  } // end main method
+} // end class HelloWorld
